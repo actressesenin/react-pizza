e3a608517edb9be49fcd3a9de94594413c11ff78
Evgeniy Zhukovskiy <zh3nya.zhukovskiy@yandex.ru>
2023-05-26 21:36:23 +0300

    all

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './redux/slices/filterSlice';
 import './App.css';
 import './scss/app.scss';
 import Header from './components/Header';
